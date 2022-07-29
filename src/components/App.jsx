f907a2d22eab86c78479c9a566b15a218091af73
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2022-07-30 01:23:52 +0530

    Dynamic Entry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,23 @@ import react from "react";
 import Header from "./Header"
 import Note from "./Note"
 import Footer from "./Footer"
+import notes from "../notes"
 
+console.log(notes);
+
+function createNotes(noteItem)  {
+  return (
+    <Note
+      key={noteItem.id}
+      title = {noteItem.title}
+      content = {noteItem.content}
+    />
+  );
+}
 
 function App() {
     return <div><Header/>
-    <Note/>
+    {notes.map(createNotes)}
     <Footer/></div>
 }
 
